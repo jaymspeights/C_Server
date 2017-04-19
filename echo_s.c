9ca6191e40811b68bd2ffe1312531bb1b5b034cc
jaymspeights <jaymspeights@gmail.com>
2017-04-19 02:43:58 -0500

    cleaned code. still needs full testing

--- echo_s.c
+++ echo_s.c
@@ -39,15 +39,12 @@ int main(int argc, char *argv[]) {
      socklen_t clilen;
      int buffer_length = 256;
      char buffer[buffer_length];
-     struct sockaddr_in serv_addr_tcp, serv_addr_udp, cli_addr;
-     int n, pid, num_ready;
+     struct sockaddr_in serv_addr, cli_addr;
+     int n, pid;
      fd_set fset;
 
      //hard-coded port for server -> log_server communication
      int log_portno = 9999;
-
-     //catches SIGCHLD signal (child dies) and calls SigCatcher
-     signal(SIGCHLD,SigCatcher);
 
      //check for command line arguments
      if (argc < 2) {
@@ -61,30 +58,29 @@ int main(int argc, char *argv[]) {
      udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
      if (udp_fd < 0)
         error("ERROR opening socket");
-     bzero((char *) &serv_addr_udp, sizeof(serv_addr_udp));
-     serv_addr_udp.sin_family = AF_INET;
-     serv_addr_udp.sin_addr.s_addr = INADDR_ANY;
-     serv_addr_udp.sin_port = htons(portno);
-
-     if (bind(udp_fd, (struct sockaddr *) &serv_addr_udp,
-              sizeof(serv_addr_udp)) < 0)
-              error("ERROR on binding");
-
 
      //init tcp socket
      tcp_fd = socket(AF_INET, SOCK_STREAM, 0);
      if (tcp_fd < 0)
         error("ERROR opening socket");
-     bzero((char *) &serv_addr_tcp, sizeof(serv_addr_tcp));
-     serv_addr_tcp.sin_family = AF_INET;
-     serv_addr_tcp.sin_addr.s_addr = INADDR_ANY;
-     serv_addr_tcp.sin_port = htons(portno);
 
-     if (bind(tcp_fd, (struct sockaddr *) &serv_addr_tcp,
-              sizeof(serv_addr_tcp)) < 0)
+     //init server address
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+
+     //bind sockets
+     if (bind(udp_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+              error("ERROR on binding");
+
+     if (bind(tcp_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
               error("ERROR on binding");
 
      listen(tcp_fd,5);
+
+     //catches SIGCHLD signal (child dies) and calls SigCatcher
+     signal(SIGCHLD,SigCatcher);
 
      //reset the fd_set
      FD_ZERO(&fset);
@@ -94,7 +90,7 @@ int main(int argc, char *argv[]) {
           FD_SET(tcp_fd, &fset);
           FD_SET(udp_fd, &fset);
           //if no sockets are ready and
-          if ((num_ready = select(maxfd, &fset, NULL, NULL, NULL)) < 0) {
+          if ((select(maxfd, &fset, NULL, NULL, NULL)) < 0) {
                //if interrupt flag is set, try again
                if (errno == EINTR)
                     continue;
